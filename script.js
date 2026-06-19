3603f49da2a296274993ad5f6c8d7686f12021fb
officialzd1 <zd1.zeyad@gmail.com>
2026-06-19 05:13:04 +0300

    1

--- script.js
+++
@@ -1,6 +0,0 @@
-
-window.onscroll = function() {
-    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    document.getElementById("scrollProgress").style.width = (winScroll / height) * 100 + "%";
-};
