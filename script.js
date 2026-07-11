b23a23bce1504ccab77aa5445dfea65bb23122ea
officialzd1 <zd1.zeyad@gmail.com>
2026-07-11 03:55:44 +0300

    1

--- script.js
+++ script.js
@@ -329,8 +329,8 @@ stages: [
     
 "التنزيل Downloading <span style='color: #00ff22;'>100%</span>",
 "الترتيب Organizing <span style='color: #00ff22;'>100%</span>",
-"القص Cutting <span style='color: #e67e22;'>48%</span>",
-"الانتقالات Transitions <span style='color: #e67e22;'>48%</span>",
+"القص Cutting <span style='color: #e67e22;'>74%</span>",
+"الانتقالات Transitions <span style='color: #e67e22;'>74%</span>",
 "التصدير Exporting <span style='color: #ffffff;'>0%</span>",
 "التسليم ✓ Final Delivery",
 
