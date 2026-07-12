a3a36f9b055f1fbe43f4a44cd4ce6c5ad5f3ed88
officialzd1 <zd1.zeyad@gmail.com>
2026-07-12 06:59:56 +0300

    1

--- script.js
+++ script.js
@@ -287,11 +287,11 @@ const myProjects = {
 name: " فلوق يوتيوب ",
 status: "active",
 statusMsg: " جاري العمل | In Progress ",
-currentStage: 0, 
+currentStage: 1, 
 stages: [
     
-"التنزيل Downloading <span style='color: #ffffff;'>0%</span>",
-"الترتيب Organizing <span style='color: #ffffff;'>0%</span>",
+"التنزيل Downloading <span style='color: #00ff22;'>100%</span>",
+"الترتيب Organizing <span style='color: #e67e22;'>50%</span>",
 "القص Cutting <span style='color: #ffffff;'>0%</span>",
 "الانتقالات Transitions <span style='color: #ffffff;'>0%</span>",
 "التصدير Exporting <span style='color: #ffffff;'>0%</span>",
