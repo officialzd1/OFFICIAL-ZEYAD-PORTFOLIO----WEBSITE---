ab76571a4144e1966a8822c8dbd981cc6b20e338
officialzd1 <zd1.zeyad@gmail.com>
2026-09-24 21:05:22 +0300

    ZD1

--- script.js
+++ script.js
@@ -227,9 +227,9 @@ const myProjects = {
 
     "ZD-491": { 
         name: "عرض وزارة الدفاع 2026",
-        deliveryDate: "8:30PM | 24 September 2026",
-        currentStage: 11, 
-        driveUrl: "https://drive.google.com/drive/folders/182xx24cuvOIvoH4Gw_Yr_rykjG4ohn2N?usp=sharing",
+        deliveryDate: "9:30PM | 24 September 2026",
+        currentStage: 12, 
+        driveUrl: "",
         stages: [
             { ar: "التنزيل", en: "Downloading" },
             { ar: "الترتيب", en: "Organizing" },
