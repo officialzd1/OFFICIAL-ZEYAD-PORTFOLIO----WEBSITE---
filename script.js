52670b245b8e2df43bb48620c1ce7453579c3c3d
officialzd1 <zd1.zeyad@gmail.com>
2026-09-26 01:49:54 +0300

    ZD1

--- script.js
+++ script.js
@@ -227,7 +227,7 @@ const myProjects = {
 
     "ZD-553": { 
         name: "كأس الخليج 27 - 2026",
-        deliveryDate: "06:18PM | 25 September 2026",
+        deliveryDate: "06:18PM | 26 September 2026",
         currentStage: 0, 
         driveUrl: "",
         stages: [
